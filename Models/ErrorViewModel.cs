49d35f3229d6d3e4cda087f51fea6ece2900c07d
khan1542 <kan.mgk@gmail.com>
2019-12-02 01:48:48 +0500

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculation_for_drying_sand.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
